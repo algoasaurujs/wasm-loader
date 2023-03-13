aa484a448ca701b72fba7a36ce5b4e00eb195229
Ehsan Samavati <samaavaati@gmail.com>
2023-03-14 01:22:29 +0300

    fix: type refrence bug fixed

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "wasm-loader",
-      fileName: "wasm-loader",
+      fileName: "index",
     },
     rollupOptions: {
       external: ["fs", "path"]
